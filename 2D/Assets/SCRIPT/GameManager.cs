606f14879bc7d7edfd0ef67e41b123a602498791
a4608635 <57396749+a4608635@users.noreply.github.com>
2019-11-21 21:37:17 +0800

    延遲重複調用#注意練習喔

--- 2D/Assets/SCRIPT/GameManager.cs
+++ 2D/Assets/SCRIPT/GameManager.cs
@@ -8,6 +8,30 @@ public class GameManager : MonoBehaviour
     [Header("最高分數")]
     public int fraction2;
 
+    // GameObject 可以存放預置物以及廠景上的物件
+    [Header("水管")]
+    public GameObject pipe;
+
+    /// <summary>
+    /// 生成水管
+    /// </summary>
+    public void SpawnPipe()
+    {
+        // 因為有繼承此類別才可簡寫Object
+        // Object.Instantiate(pipe);
+
+        // 浮點數 = 隨機.靜態方法(最小,最大)
+        float y = Random.Range(-0.2f, 2f);
+
+        // 三維向量 Vector3 - x, y, z
+        Vector3 pos = new Vector3(4.1F, y, 0);
+
+        // 四元數 Quaternion - x, y, z, w
+        Quaternion rot = new Quaternion(0, 0, 0, 0);
+
+        Instantiate(pipe, pos, rot);
+    }
+
     /// <summary>
     /// 加分功能
     /// </summary>
@@ -32,6 +56,9 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        
+        //延遲重複調用 
+        //語法: 延遲重複調用=名稱,時間,延遲時間
+        InvokeRepeating("SpawnPipe", 0 , 1);
     }
+
 }
---
+++ 2D/Assets/SCRIPT/LeadAPI.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class LeadAPI : MonoBehaviour
+{
+    private void Start()
+    {
+        print("信教啦!");
+
+        // object 任何類型：int、float、string、bool
+        Debug.Log("你敢不信教!!!!");
+
+        // 靜態成員的取得方法
+        print(Random.value);
+
+        // 練習取得PI
+        print(Mathf.PI);
+
+        // 靜態方法： 類別.方法(對應的參數);
+        print ("隨機範圍:"+Random.Range( 130.1F,260.9f));
+
+        // object 任何類型：int、float、string、bool
+        Debug.Log("我不想學英文阿!");
+        Debug.LogWarning("當心英文");
+        Debug.LogError("英文錯了阿!");
+    }
+
+    //Update=60禎一直觸發 用來重覆偵測觸發
+    private void Update()
+    {
+        // 輸入.按鍵名稱
+        print(Input.inputString);
+
+        // 練習：請練習偵測玩家有沒有按 s
+        print(Input.GetKeyDown("s"));
+    }
+}
